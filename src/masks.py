0609f28e8dadb3402035ce5d11249bb9288ca7a1
Agata <agagor1961@gmail.com>
2024-05-09 14:51:13 +0300

    lints settings created

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def card_mask(card_number: Union[str, int]) -> str:
+    """Функция принимает номер карты и возвращает его в замаскированном виде"""
+    card_number = str(card_number)
+    masked_card_number = f"{card_number[0: 4]} {card_number[4: 6]}** **** {card_number[-4:]}"
+    return masked_card_number
+
+
+def account_mask(account_number: Union[str, int]) -> str:
+    """Функция принимает номер счета и возвращает его в замаскированном виде"""
+    account_number = str(account_number)
+    masked_account_number = f"**{account_number[-4:]}"
+    return masked_account_number
