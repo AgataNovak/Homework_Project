261c6f7a1560478367726b7a5164455ff09ae4c1
Agata <agagor1961@gmail.com>
2024-05-09 15:45:52 +0300

    func created

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from src.masks import account_mask, card_mask
+
+
+def card_or_account_mask(number: str) -> str:
+    """Функция принимает строку с типом и номером карты или счёта, и возвращает эти данные в замаскированном виде"""
+
+    account_numbers = []
+    card_numbers = []
+
+    if number[-5].isdigit() == True:
+        """Цикл, определяющий, с какими входными данными работает функция: номер карты или счёта"""
+
+        account_numbers.append(number[-20:])
+        account_numbers = "".join(account_numbers)
+        masked_account = account_mask(account_numbers)
+        return f"{number[:4]} {masked_account}"
+
+    elif number[-5] == " ":
+        card_numbers.append(number[-19:])
+        card_numbers = "".join(card_numbers)
+        card_numbers = card_numbers.replace(" ", "")
+        masked_card = card_mask(card_numbers)
+        return f"{number[:-19]} {masked_card}"
---
+++ tests/main.py
@@ -0,0 +1,4 @@
+from src.widget import card_or_account_mask
+
+print(card_or_account_mask("Счет 73654108430135874305"))
+print(card_or_account_mask("Visa Platinum 7000 7922 8960 6361"))
