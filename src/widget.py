35fc5e81bb7fb9e8be213d5dca6a9bd3558ecbdd
Agata <agagor1961@gmail.com>
2024-05-09 16:02:19 +0300

    small fixes

--- src/widget.py
+++ src/widget.py
@@ -25,6 +25,3 @@ def card_or_account_mask(number: str) -> str:
         result = f"{number[:-19]} {masked_card}"
 
     return result
-
-
-print(card_or_account_mask("Visa Platinum 7000 7922 8960 6361"))
