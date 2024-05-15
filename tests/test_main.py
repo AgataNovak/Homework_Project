e7694236095ad391a87f74554be47de27e5aba87
Agata <agagor1961@gmail.com>
2024-05-15 22:47:22 +0300

    fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,19 @@
 import pytest
+
 from src.widget import card_or_account_mask, date_formating
 
 
 def test_account_mask(number_of_account):
-    assert card_or_account_mask(number_of_account) == 'Счет **4305'
+    assert card_or_account_mask(number_of_account) == "Счет **4305"
 
 
 def test_card_mask(number_of_card):
-    assert card_or_account_mask(number_of_card) == 'Visa Platinum  7000 79** **** 6361'
+    assert card_or_account_mask(number_of_card) == "Visa Platinum  7000 79** **** 6361"
 
 
 def test_card_or_account_mask_empty_string():
     with pytest.raises(IndexError):
-        assert card_or_account_mask('')
+        assert card_or_account_mask("")
 
 
 def test_card_or_account_mask_no_arg():
@@ -21,4 +22,4 @@ def test_card_or_account_mask_no_arg():
 
 
 def test_date_formating(date_old_format):
-    assert date_formating(date_old_format) == '11.07.2018'
+    assert date_formating(date_old_format) == "11.07.2018"
