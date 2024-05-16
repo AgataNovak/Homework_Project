466be72dfaf2f00d80f7ee5ca22b3a20002a003b
Agata <agagor1961@gmail.com>
2024-05-17 02:36:24 +0300

    add test. add README file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,5 @@ import pytest
 
 
 @pytest.fixture
-def number_of_account():
-    return "Счет 73654108430135874305"
-
-
-@pytest.fixture
-def number_of_card():
-    return "Visa Platinum 7000 7922 8960 6361"
-
-
-@pytest.fixture
-def date_old_format():
-    return "2018-07-11T02:26:18.671407"
+def nothing():
+    return None
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,23 +3,40 @@ import pytest
 from src.widget import card_or_account_mask, date_formating
 
 
-def test_account_mask(number_of_account):
-    assert card_or_account_mask(number_of_account) == "Счет **4305"
+@pytest.mark.parametrize(
+    "number_of_account_or_card, expected",
+    [
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Счет 12345678901234567890", "Счет **7890"),
+        ("Счет 00000000000000000000", "Счет **0000"),
+        ("Visa Platinum 7000 7922 8960 6361", "Visa Platinum  7000 79** **** 6361"),
+        ("Visa Platinum 1234 5678 9012 3456", "Visa Platinum  1234 56** **** 3456"),
+        ("Visa Master Card 0000 0000 0000 0000", "Visa Master Card  0000 00** **** 0000"),
+        ("GasPromBank 1234 1234 1234 1234", "GasPromBank  1234 12** **** 1234"),
+    ],
+)
+def test_card_or_account_mask(number_of_account_or_card, expected):
+    assert card_or_account_mask(number_of_account_or_card) == expected
 
 
-def test_card_mask(number_of_card):
-    assert card_or_account_mask(number_of_card) == "Visa Platinum  7000 79** **** 6361"
-
-
-def test_card_or_account_mask_empty_string():
+def test_card_or_account_mask_empty_string(nothing):
     with pytest.raises(IndexError):
         assert card_or_account_mask("")
 
 
-def test_card_or_account_mask_no_arg():
-    with pytest.raises(TypeError):
-        assert card_or_account_mask()
+@pytest.mark.parametrize(
+    "date_old_format, expected",
+    [
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2018-06-30T02:08:58.425572", "30.06.2018"),
+        ("2018-09-12T21:27:25.241689", "12.09.2018"),
+        ("2018-10-14T08:21:33.419441", "14.10.2018"),
+    ],
+)
+def test_date_formating(date_old_format, expected):
+    assert date_formating(date_old_format) == expected
 
 
-def test_date_formating(date_old_format):
-    assert date_formating(date_old_format) == "11.07.2018"
+def test_date_formating_empty(nothing):
+    with pytest.raises(IndexError):
+        assert card_or_account_mask("")
