4cb8c4abda4ee74292ea50694f217450d2501153
Agata <agagor1961@gmail.com>
2024-05-17 09:59:42 +0300

    add new python.file module "generators", README file updated, small fixes in main.py file

---
+++ src/generators.py
@@ -0,0 +1,43 @@
+def filter_by_currency(list_of_transactions, currency):
+    """Функция принимает список данных о транзакциях и валюту, и возвращает список транзакций с указанной валютой"""
+
+    transactions_by_currency = list(
+        transaction
+        for transaction in list_of_transactions
+        if transaction["operationAmount"]["currency"]["name"] == currency
+    )
+    for transaction in transactions_by_currency:
+        yield transaction
+
+# usage example
+# usd_transactions = filter_by_currency(transactions, 'USD')
+# for i in range(3):
+#     print(next(usd_transactions)['id'])
+
+
+def transaction_descriptions(list_of_transactions):
+    """Функция которая принимает список данных о транзакцяих, и возвращает описания транзакций"""
+
+    for transaction in list_of_transactions:
+        desc = transaction["description"]
+        yield desc
+
+# usage example
+# description = transaction_descriptions(transactions)
+# for a in range(5):
+#     print(next(description))
+
+
+def card_number_generator(start, stop):
+    """Функция которая принимает диапазон номеров карт и возвращает сгенерированные номера карт"""
+
+    for i in range(start, stop):
+        length_of_zeroes = 16 - len(str(i))
+        card_number_generated = f"{'0' * length_of_zeroes}{str(i)}"
+        generated_number = (f"{card_number_generated[0: 4]} {card_number_generated[4: 8]} "
+                            f"{card_number_generated[8:12]} {card_number_generated[-4:]}")
+        yield generated_number
+
+# usage example
+# for card_number in card_number_generator(1234567, 1234569):
+#     print(card_number)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import pytest
-
+from src.main import transactions
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.widget import card_or_account_mask, date_formating
 
 
@@ -40,3 +41,33 @@ def test_date_formating(date_old_format, expected):
 def test_date_formating_empty(nothing):
     with pytest.raises(IndexError):
         assert card_or_account_mask("")
+
+
+def test_filter_by_currency_usd():
+    usd_transactions = filter_by_currency(transactions, "USD")
+    assert next(usd_transactions)["id"] == 939719570
+    assert next(usd_transactions)["id"] == 142264268
+    assert next(usd_transactions)["id"] == 895315941
+
+
+def test_filter_by_currency_rub():
+    rub_transactions = filter_by_currency(transactions, "руб.")
+    assert next(rub_transactions)["id"] == 873106923
+    assert next(rub_transactions)["id"] == 594226727
+
+
+def test_transaction_descriptions():
+    description = transaction_descriptions(transactions)
+    assert next(description) == "Перевод организации"
+    assert next(description) == "Перевод со счета на счет"
+    assert next(description) == "Перевод со счета на счет"
+    assert next(description) == "Перевод с карты на карту"
+    assert next(description) == "Перевод организации"
+
+
+def test_card_number_generator():
+    card_number_generated = card_number_generator(123456, 123460)
+    assert next(card_number_generated) == "0000 0000 0012 3456"
+    assert next(card_number_generated) == "0000 0000 0012 3457"
+    assert next(card_number_generated) == "0000 0000 0012 3458"
+    assert next(card_number_generated) == "0000 0000 0012 3459"
